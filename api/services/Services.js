367d0aa2a9ca73dbccff5bd72bab45eb30f1aca2
leandro costa <lcosta.sp.br@gmail.com>
2022-07-06 07:25:55 -0300

    refactor(aula5): :art: implements generic methods

--- api/services/Services.js
+++ api/services/Services.js
@@ -3,10 +3,27 @@ const database = require('../models')
 class Services {
     constructor(nomeModelo) {
         this.nomeModelo = nomeModelo
+        this.modelo = database[this.nomeModelo]
     }
 
     async obterTodosOsRegistros() {
-        return database[this.nomeModelo].findAll()
+        return this.modelo.findAll()
+    }
+
+    async obterRegistroPorID(id) {
+        return this.modelo.findOne({ where: { id: Number(id) } })
+    }
+
+    async criarRegistro(registro) {
+        return this.modelo.create(registro)
+    }
+
+    async apagarRegistro(id) {
+        return this.modelo.destroy({ where: { id: Number(id) } })
+    }
+
+    async atualizarRegistro(registro, id) {
+        return this.modelo.update(registro, { where: { id: Number(id) } })
     }
 }
 
