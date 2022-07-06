bff950a4181da6d53688137cdfd34ee8169f2b75
leandro costa <lcosta.sp.br@gmail.com>
2022-07-06 07:43:15 -0300

    refactor(aula5): :art: create a single file to exports all services

---
+++ api/services/index.js
@@ -0,0 +1,9 @@
+const PessoasService = require('./PessoasService')
+const TurmasService = require('./TurmasService')
+const NiveisService = require('./NiveisService')
+
+module.exports = {
+    PessoasService: PessoasService,
+    TurmasService: TurmasService,
+    NiveisService: NiveisService
+}
